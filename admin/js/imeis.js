ebc28820e6e67597e89f14aa05491f1f6fcf29ed
Luis Rodríguez <soluciones2000@gmail.com>
2023-07-27 18:39:56 -0400

    Más pruebas

--- admin/js/imeis.js
+++ admin/js/imeis.js
@@ -100,7 +100,7 @@ function handleFile(e) {
                method: 'POST',
                body: datos
             })
-            .then((response) => {console.log(response); response.json();})
+            .then((response) => response.json())
             .then((responseData) => {
                if (responseData.exito=="SI") {
                   alert(responseData.mensaje);
